5702f5f02c564e6c935823348e9030a0cee262d1
ryutaro1223 <k.ryutaro1223@gmail.com>
2021-06-26 10:44:45 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,8 @@ Rails.application.routes.draw do
 
   end
 
-  resources :contacts, only: [:new, :create]
+
+  resources :contacts, only: [:new, :create,]
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
