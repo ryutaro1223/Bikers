d40aa135f713ac70fa15e676553c264a5420dfef
ryutaro1223 <k.ryutaro1223@gmail.com>
2021-06-28 10:15:20 +0000

    letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   # 問い合わせフォーム用
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+
 
   devise_for :users
   get "home/about", to: "homes#about"
