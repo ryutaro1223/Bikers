317c6e0833b10f38e1dc3a9646b1baf905b8bfd0
ryutaro1223 <k.ryutaro1223@gmail.com>
2021-06-28 05:55:33 +0000

    gem使わずに権限付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_094354) do
+ActiveRecord::Schema.define(version: 2021_06_28_053728) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment_content"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_094354) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
